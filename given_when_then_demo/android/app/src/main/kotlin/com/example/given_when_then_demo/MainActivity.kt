3f197946138eb2b24f81c389d5bdbc8ce0808b35
Fred Grott <fred.grott@gmail.com>
2024-06-02 13:07:39 -0500

    first commit
    first commit

---
+++ given_when_then_demo/android/app/src/main/kotlin/com/example/given_when_then_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.given_when_then_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ shouldly_demo/android/app/src/main/kotlin/com/example/shouldly_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shouldly_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
